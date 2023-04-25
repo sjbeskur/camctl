be050dc0b28ba40423168438ab383433b1bf93e9
sjbeskur <sam.beskur@gmail.com>
2023-04-24 23:48:35 -0600

    (wip) cli args

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-
-use clap::{Parser};
+use clap::Parser;
 mod cli;
-
-
+use arducam_mipicamera::Encoding;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     let args = cli::Config::parse();
-    
-    camctl::capture(1)?;
+
+    let mut enc = match args.encoding {
+        cli::ImageEncoding::JPEG => Encoding::Jpeg,
+        _ => Encoding::Png,
+    };
+
+    camctl::capture(1, args.mode, args.timeout, enc)?; //, args.encoding)?;
     Ok(())
 }
-
-
